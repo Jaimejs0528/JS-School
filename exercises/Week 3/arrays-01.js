9a3aee93ccb777b5acfc209672f66acb5d0951ed
Jaime Jimenez Serrano <jaimejimenezisi@gmail.com>
2018-11-02 18:01:49 -0500

    linting exercises, still lack storage-01.js

--- exercises/Week 3/arrays-01.js
+++ exercises/Week 3/arrays-01.js
@@ -1,12 +1,14 @@
+// eslint-disable-next-line func-names
 const appSort = function () {
-    const myArray = [2, 32, 53, 1, 33, 12, 50]; // array to sort
-    /*
+  const myArray = [2, 32, 53, 1, 33, 12, 50]; // array to sort
+  /*
     The next line sort the variable myArray using the rule current - next, where
     if the result is negative means that current is minor than next, if the result
     is positive means that next is minor than current and if the result is 0 means
     that both numbers are equals.
     */
-    const sorted = myArray.sort((current, next) => current - next);
-    console.log(sorted); // Print in console the array already sorted
-}
+  const sorted = myArray.sort((current, next) => current - next);
+  // eslint-disable-next-line no-console
+  console.log(sorted); // Print in console the array already sorted
+};
 appSort();
--- exercises/Week 3/time-01.js
+++ exercises/Week 3/time-01.js
@@ -1,24 +1,24 @@
-//Avoiding use global variables
-const appCountDown = function () {
-    let time = 300; // time to countdown
-// timer that execute the function counterDown every 1000 milliseconds(1 second)
-    const interval = setInterval(counterDown, 1000);
-
-    /*
+// Avoiding use global variables
+const appCountDown = function main() {
+  let time = 300; // time to countdown
+  let interval;
+  // timer that execute the function counterDown every 1000 milliseconds(1 second)
+  /*
     Function that shows every second how many time reminds for time arrives to zero
     * */
-    function counterDown() {
-        if (time >= 0) {
-            let minutes = Math.floor(time / 60);
-            let seconds = time % 60;
-            minutes = minutes < 10 ? `0${minutes}` : minutes;
-            seconds = seconds < 10 ? `0${seconds}` : seconds;
-            console.log(`${minutes}:${seconds}`);
-            time--;
-        } else {
-            clearInterval(interval); // Stop the interval that is running over this function
-        }
+  function counterDown() {
+    if (time >= 0) {
+      let minutes = Math.floor(time / 60);
+      let seconds = time % 60;
+      minutes = minutes < 10 ? `0${minutes}` : minutes;
+      seconds = seconds < 10 ? `0${seconds}` : seconds;
+      // eslint-disable-next-line no-console
+      console.log(`${minutes}:${seconds}`);
+      time -= 1;
+    } else {
+      clearInterval(interval); // Stop the interval that is running over this function
     }
-}
+  }
+  interval = setInterval(counterDown, 1000);
+};
 appCountDown();
-
