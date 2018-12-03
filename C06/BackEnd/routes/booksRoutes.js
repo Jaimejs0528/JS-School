47c327251105a698e23c6ffb4cb4b9c6d3eee511
Jaime Jimenez Serrano <jaimejimenezisi@gmail.com>
2018-12-03 17:52:10 -0500

    temporary revert change

--- C06/BackEnd/routes/booksRoutes.js
+++ C06/BackEnd/routes/booksRoutes.js
@@ -2,12 +2,12 @@ const bookController = require('../controller/bookController');
 const userController = require('../controller/userController');
 
 exports.bookRoutes = function bookRoutes(app) {
-  app.get('/bookshelf/books', userController.isLogin, bookController.findAllBooks); // Gets all books
-  app.get('/bookshelf/books/digitals', userController.isLogin, bookController.getDigitals); // Gets books in digital
+  app.get('/bookshelf/books', bookController.findAllBooks); // Gets all books
+  app.get('/bookshelf/books/digitals', bookController.getDigitals); // Gets books in digital
   app.patch('/bookshelf/books/digitals/lends', userController.isLogin, bookController.getDigitals); // Gets books in digital
-  app.get('/bookshelf/books/:isbn', userController.isLogin, bookController.findByISBN); // Gets a book by isbn
-  app.get('/bookshelf/books/cities/:city', userController.isLogin, bookController.findByCity); // Gets books by city
-  app.post('/bookshelf/books', userController.isLogin, bookController.addOneBook); // Add a new book
+  app.get('/bookshelf/books/:isbn', bookController.findByISBN); // Gets a book by isbn
+  app.get('/bookshelf/books/cities/:city', bookController.findByCity); // Gets books by city
+  app.post('/bookshelf/books',  bookController.addOneBook); // Add a new book
   app.patch('/bookshelf/books/lends', userController.isLogin, bookController.lendABook); // Lend a book
   app.post('/bookshelf/books/lends', userController.isLogin, bookController.findByUserLoans); // Gets books loans by specific user
 };
